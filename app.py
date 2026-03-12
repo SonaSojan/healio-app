7d28923917665619ded2e7a00c75de8531205f6f
SonaSojan <sonasojan107@gmail.com>
2026-03-12 21:32:13 +0530

    update app.py for deployment

--- app.py
+++ app.py
@@ -61,5 +61,8 @@ def doctor():
 
     return render_template("doctor.html", messages=messages)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
